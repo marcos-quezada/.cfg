857c4a50359ea61f71e16336b7637b8b25f5405b
Marcos Quezada Perez <marcos.quezadaperez@peakwork.com>
2019-09-27 15:59:43 +0200

    Re-organization of colors.

--- .zsh/plugins_after.zsh
+++ .zsh/plugins_after.zsh
@@ -8,20 +8,20 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
 if [[ "$(tput colors)" == "256" ]]; then
     ZSH_HIGHLIGHT_STYLES[default]=none
-    ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=160
+    ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=202
     ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=037,bold #,standout
-    ZSH_HIGHLIGHT_STYLES[alias]=fg=045,bold
-    ZSH_HIGHLIGHT_STYLES[builtin]=fg=045,bold
-    ZSH_HIGHLIGHT_STYLES[function]=fg=045,bold
-    ZSH_HIGHLIGHT_STYLES[command]=fg=045,bold
-    ZSH_HIGHLIGHT_STYLES[precommand]=fg=045,underline
+    ZSH_HIGHLIGHT_STYLES[alias]=fg=006,bold
+    ZSH_HIGHLIGHT_STYLES[builtin]=fg=006,bold
+    ZSH_HIGHLIGHT_STYLES[function]=fg=006,bold
+    ZSH_HIGHLIGHT_STYLES[command]=fg=006,bold
+    ZSH_HIGHLIGHT_STYLES[precommand]=fg=006,underline
     ZSH_HIGHLIGHT_STYLES[commandseparator]=none
     ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=037
     ZSH_HIGHLIGHT_STYLES[path]=fg=166,underline
     ZSH_HIGHLIGHT_STYLES[globbing]=fg=033
     ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=white,underline
-    ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=006,bold
-    ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=006,bold
+    ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=045,bold
+    ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=045,bold
     ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=none
     ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=136
     ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=136
